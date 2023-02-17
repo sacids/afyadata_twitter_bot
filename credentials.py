ae16d83b7aecbc22b13adca57cd02f9f967528ed
renfrid <ngolongolo@multics.co.tz>
2023-02-17 09:36:16 +0300

    fixed gitignore

--- credentials.py
+++
@@ -1,8 +0,0 @@
-## credentials contain:
-consumer_key        = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
-consumer_secret     = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
-access_token        = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
-access_token_secret = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
-user_id		    = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
-
-API_BASE            = 'ems/api/'
